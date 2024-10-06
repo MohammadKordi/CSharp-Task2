e6f40a936f8c5680c7805bbc2c93c353d257d97a
MohammadKordi <mohkordi21@gmail.com>
2024-10-06 19:50:20 +0300

    Add files via upload

---
+++ Task2-2/Task2-2/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Q 1
+
+            //Console.WriteLine("Inter Your Number 1 : \n");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Inter Your Number 2 : \n");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //if ( num1 > num2 )
+            //{
+            //    Console.WriteLine("The smaller Number 2 = " + num2); 
+            //}
+
+            //else if (num1 < num2)
+            //{
+            //    Console.WriteLine("The smaller Number 1 = " + num1);
+            //}
+
+
+            // Q 2
+
+            //Console.WriteLine("Inter Your Number : \n");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //if (number > 0)
+            //{
+            //    Console.WriteLine("the sign is + ") ; 
+            //}
+            //else if (number < 0)
+            //{
+            //    Console.WriteLine("the sign is - ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Number = 0");
+            //}
+
+
+            // Q 3
+
+            //Console.Write("Inter Number 1 : \n");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Inter Number 2 : \n");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Inter Number 3 : \n");
+            //int num3 = Convert.ToInt32(Console.ReadLine());
+
+
+            //if (num1 >= num2 && num1 >= num3)
+            //{
+            //    if (num2 >= num3)
+            //        Console.WriteLine($"{num1} {num2} {num3}");
+            //    else
+            //        Console.WriteLine($"{num1} {num3} {num2}");
+            //}
+            //else if (num2 >= num1 && num2 >= num3)
+            //{
+            //    if (num1 >= num3)
+            //        Console.WriteLine($"{num2} {num1} {num3}");
+            //    else
+            //        Console.WriteLine($"{num2} {num3} {num1}");
+            //}
+            //else
+            //{
+            //    if (num1 >= num2)
+            //        Console.WriteLine($"{num3} {num1} {num2}");
+            //    else
+            //        Console.WriteLine($"{num3} {num2} {num1}");
+
+
+
+
+            // Q 4 
+
+
+            //Console.Write("Inter Number 1 : \n");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Inter Number 2 : \n");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Inter Number 3: \n");
+            //int num3 = Convert.ToInt32(Console.ReadLine());
+
+            //int MaxNumber;
+
+
+            //if (num1 >= num2 && num1 >= num3)
+            //{
+            //    MaxNumber = num1;
+            //}
+            //else if (num2 >= num1 && num2 >= num3)
+            //{
+            //    MaxNumber = num2;
+            //}
+            //else
+            //{
+            //    MaxNumber = num3;
+            //}
+
+
+            //Console.WriteLine($"The Max Number = {MaxNumber}");
+
+
+            // Q 7
+
+            //Console.Write("Enter the number of minutes: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //int hours = number / 60;
+            //int minutes = number % 60;
+
+            //Console.WriteLine($"{hours} Hours , {minutes} Minutes");
+
+
+            // Q 8
+
+            //string[] text =
+            //{
+            //"Mohammad Al Kordi",
+            //"Orange Coding School",
+            //"Jadara Uinversty",
+            //"Computer Since",
+            //"Hello World"
+            //};
+
+
+            //int fixedLength = 40; 
+
+            //foreach (string sentence in text)
+            //{
+            //    Console.WriteLine(sentence.PadRight(fixedLength).Substring(0, fixedLength));
+
+        }
+    }
+}
+
